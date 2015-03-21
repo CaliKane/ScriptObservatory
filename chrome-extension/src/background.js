bb96b5506540fd8563d8b62b4bbe5b51ac4eb26d
Andy Martin <andy@andymartin.cc>
2015-03-21 13:11:54 -0700

    major clean up for background.js --> use SCRIPTS to track scripts observed and tabs.onUpdated listener to perform the POST request

--- chrome-extension/src/background.js
+++ chrome-extension/src/background.js
@@ -11,14 +11,26 @@
 
 /* 
  * CONSTANTS
+ * ---------
+ * (1) API_BASE_URL: The base URL for the Pageview API.
  */
 API_BASE_URL = "https://scriptobservatory.org/api/pageview";
-SCRIPT_OBJECTS_TABLE = [];
-MAX_PAGES = 10;
 
 
 /*
- * httpGet(url) - Perform a HTTP GET request to *url* and return its content
+ * GLOBAL DATA STRUCTURES
+ * ----------------------
+ * (1) SCRIPTS: Maps the tabId to a list of all scripts loaded for the given tab. Cleared every
+ *              time a request for a main_frame is made. Used & deleted from memory every time 
+ *              the chrome.tabs.onUpdated listener fires and data is POSTed to API.
+ */
+SCRIPTS = {};
+
+
+/*
+ * FUNCTION httpGet(url)
+ * ---------------------
+ * Perform a HTTP GET request to *url* and return its content
  */
 function httpGet(url){
     var xmlHttp = new XMLHttpRequest();
@@ -29,7 +41,9 @@ function httpGet(url){
 
 
 /*
- * httpPost(url, data) - Send json-ified *data* with a HTTP POST request to *url*
+ * FUNCTION httpPost(url, data)
+ * ----------------------------
+ * Send json-ified *data* with a HTTP POST request to *url*
  */
 function httpPost(url, data){
     var request = new XMLHttpRequest();
@@ -40,27 +54,19 @@ function httpPost(url, data){
 }
 
 
-/*
- * makeIdString(tabId, frameId)) - Create a unique ID string from *tabId* and *frameId*
- *
- * this is used to try to detect the parent frame that is responsible for a given request.
- * frameId is only unique on a per-tab basis, so both are needed.
- *
- * more info: https://developer.chrome.com/extensions/webRequest
- */
-function makeIdString(tabId, frameId){
-    return tabId + "-" + frameId;
-}
-
-
 /* 
- * We hook into chrome.webRequest.onBeforeRequest to perform our own download of the content and calculate the
- * sha256 hash of the content. After the download is complete, we POST the url & hash value to *API_BASE_URL* 
- * and embed the content in the webpage by changing the URL to a data URI object (avoids downloading content twice).
+ * FUNCTION added to chrome.webRequest.onBeforeRequest listeners
+ * -------------------------------------------------------------
+ * We hook into chrome.webRequest.onBeforeRequest to keep track of the tabIds of "main_frame"
+ * requests and to grab the content of "script" requests. For "script" requests, we perform our 
+ * own download of the content and calculate the sha256 hash of what we receive from the server.
+ * After the download of a "script" object is complete, if an entry is present in SCRIPTS for 
+ * our current tabId, we add the data we have (script URL & hash) to the SCRIPTS data structure.
  * 
- * It would be nice if we could let the browser do the request normally and grab the content of its response,
- * but this is not currently possible with the chrome APIs. 
- *
+ * It would be nice if we could let the browser do the request for "script" objects normally 
+ * and grab the content of the response it receives, but this is not currently possible with 
+ * the chrome APIs. We're stuck injecting in this non-optimal way for now.
+ * 
  * This functionality is discussed in the following issue: 
  *   https://code.google.com/p/chromium/issues/detail?id=104058
  * 
@@ -74,88 +80,59 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         var data = "";
         var hash = "";
-        var parent_url = "";
+        var tabId = details.tabId;
 
-        var id_string = makeIdString(details.tabId, details.frameId);
-        var parent_id_string = makeIdString(details.tabId, details.parentFrameId);
-
-        console.log(details.url + " --> " + id_string + " " + parent_id_string);        
+        console.log(details.url + " --> " + tabId);
 
         if (details.type == "main_frame"){
-            MAIN_FRAME_URLS[id_string] = details.url;  // id_string --> url
-            MAIN_FRAME_URLS[parent_id_string] = details.url;  // id_string --> url
-        
-            SCRIPTS[details.url] = [];
+            SCRIPTS[tabId] = []; 
         }
-        else {
-            PARENTS[id_string] = parent_id_string;  // sub_frame id_string --> parent's id_string
-        }
-
-        while (id_string in PARENTS){
-            id_string = PARENTS[id_string];
-        }
-
-        // debug:
-        if (!(id_string in MAIN_FRAME_URLS)){
-            console.log("root id_string of " + id_string + " found for " + details.url + " but main_frame not found!!");
-        }
-        root_url = MAIN_FRAME_URLS[id_string];
-
-        if (details.type == "script"){
+        else if (details.type == "script"){
             data = httpGet(details.url);
             hash = CryptoJS.SHA256(data).toString(CryptoJS.enc.Base64);
-            SCRIPTS[root_url].push({"url": details.url, "hash": hash});
-        }
- 
 
-        // we want to redirect to the data we received if the object is a script,
-        // otherwise just be done and let the browser make its own request
-        if (details.type == "script") {
+            if (tabId in SCRIPTS){
+                SCRIPTS[tabId].push({"url": details.url, "hash": hash});
+            }
+            else{
+                // TODO: determine why this happens and find a workaround
+                console.log("tabId of " + tabId + " found for " + details.url + " but main_frame not found!!");
+            }
+
+            // TODO: determine why btoa call fails (encoding issues?)
             return {"redirectUrl":"data:text/html;base64, " + window.btoa(data)};
         }
-        else {
-            return {cancel: false};
-        }
 
+        return {cancel: false};
     }, 
     {urls: ["<all_urls>"], types: ["script", "main_frame", "sub_frame"]}, 
     ["blocking"]
 );
 
+
+/*
+ * FUNCTION added to chrome.tabs.onUpdated listeners
+ * -------------------------------------------------
+ * We hook into chrome.tabs.onUpdated to determine when a page load has completed. 
+ *
+ * We check changeInfo.status to make sure that the onUpdated listener has been called
+ * because the status has been changed to "complete" and, if this is so, send a POST
+ * request to the API_BASE_URL containing the browsing data collected and stored in 
+ * the SCRIPTS data structure. We then delete tabId's entry from SCRIPTS.
+ */
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab){
-
         if (changeInfo.status == "complete"){
-            
             var timeStamp = new Date().getTime();
             var post_data = {"url": tab.url, 
                              "date": timeStamp,
-                             "scripts": SCRIPTS[tab.url]};
+                             "scripts": SCRIPTS[tabId]};
             
-            delete SCRIPTS[tab.url];
+            delete SCRIPTS[tabId];
 
             console.log("finished ->" + JSON.stringify(post_data));
             httpPost(API_BASE_URL, post_data);
         }
     }
 );
-
-
-
-/*
- * The current use of PARENT_URLS is an ugly hack to try to keep track of the URL of 
- * each page that could possibly spawn a javascript request later on.
- * 
- * Once the onBeforeRequest listener is expanded to grab all types of pages, the URLs
- * and parent/children relationships should be recorded there...
- * 
- * The current setup will fail in cases like news.ycombinator.com, where the initial
- * request actually returns a javascript object that then pulls down the page content.
- */
-
-PARENT_URLS = {};
-MAIN_FRAME_URLS = {};
-PARENTS = {};
-SCRIPTS = {};
-
 
