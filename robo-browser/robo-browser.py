a6676e629800f067b5522fc054c8f0a4d0c07b02
Andy Martin <andy@andymartin.cc>
2015-03-28 12:27:07 -0700

    have selenium browser re-initialize between each web page, have selenium browser wait N seconds before moving on to next page (this allows for time between onload() and when the Chrome tab is finished & reports to the API). Still need to tie the two together some way to avoid unnecessary waiting for pages that don't need it...

--- robo-browser/robo-browser.py
+++ robo-browser/robo-browser.py
@@ -7,29 +7,32 @@ import os
 import sys
 import time
 
+import selenium
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-N_SECS_BETWEEN_PAGES = 3
-N_SECS_REQ_TIMEOUT = 60
+N_SECS_TO_WAIT_AFTER_ONLOAD = 20
+N_SECS_REQ_TIMEOUT = 100
+N_SECS_BETWEEN_PAGES = 1
 URL_LIST = sys.argv[1]
-
-
-options = Options()
-options.add_argument("--load-extension={0}".format(os.environ['PATH_TO_EXTENSION']))
-
-driver = webdriver.Chrome(chrome_options=options)
-driver.set_page_load_timeout(N_SECS_REQ_TIMEOUT)
 
 for web_addr in open(URL_LIST, 'r'):
     try:
         web_addr = web_addr.strip()
+        
+        options = Options()
+        options.add_argument("--load-extension={0}".format(os.environ['PATH_TO_EXTENSION']))
+
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.set_page_load_timeout(N_SECS_REQ_TIMEOUT)
         driver.get(web_addr)
-        time.sleep(N_SECS_BETWEEN_PAGES)
-    except:
+
+        time.sleep(N_SECS_TO_WAIT_AFTER_ONLOAD)
+
+    except selenium.common.exceptions.TimeoutException:
         print("the page load timed out for {0} - continuing...".format(web_addr))
 
-driver.quit()
-
+    driver.quit()
 
