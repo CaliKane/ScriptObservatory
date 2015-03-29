0fb3ba4f20472874d1ee6aaa9433bc3f5440fcb8
andy <andy@andymartin.cc>
2015-03-28 23:19:07 -0700

    remove unused directories (img/ and tests/) and consolidate app.js and basic.js into app.js

--- backend/static/js/app.js
+++ backend/static/js/app.js
@@ -4,6 +4,29 @@
  */
 
 
+function show_about(){
+    document.getElementById('about_section').style.display="block";
+    document.getElementById('raw_section').style.display="none";
+    document.getElementById('website_section').style.display="none";
+}
+
+function show_raw(){    
+    document.getElementById('about_section').style.display="none";
+    document.getElementById('raw_section').style.display="block";
+    document.getElementById('website_section').style.display="none";
+}
+
+function show_website(){    
+    document.getElementById('about_section').style.display="none";
+    document.getElementById('raw_section').style.display="none";
+    document.getElementById('website_section').style.display="block";
+}
+
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+
 API_BASE_URL = "https://scriptobservatory.org";
 
 var app = angular.module("app", []).filter('object2Array', function() {
--- backend/tests/test_filtered_query.sh
+++
@@ -1,4 +0,0 @@
-curl \
-  -k \
-  -G \
-  https://scriptobservatory.org/api/script?q={"filters":[{"name":"parent_url","op":"like","val":"%reddit.com%"}]}
