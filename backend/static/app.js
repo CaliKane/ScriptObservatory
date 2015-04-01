49b974d0c1ec1fe4ced5834ee7f92d6f63fe3405
Andy Martin <andy@andymartin.cc>
2015-04-01 04:46:23 +0000

    remove 'raw' section from viewing options

--- backend/static/app.js
+++ backend/static/app.js
@@ -6,19 +6,11 @@
 
 function show_about(){
     document.getElementById('about_section').style.display="block";
-    document.getElementById('raw_section').style.display="none";
-    document.getElementById('website_section').style.display="none";
-}
-
-function show_raw(){    
-    document.getElementById('about_section').style.display="none";
-    document.getElementById('raw_section').style.display="block";
     document.getElementById('website_section').style.display="none";
 }
 
 function show_website(){    
     document.getElementById('about_section').style.display="none";
-    document.getElementById('raw_section').style.display="none";
     document.getElementById('website_section').style.display="block";
 }
 
