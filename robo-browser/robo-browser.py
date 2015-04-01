b22daa6373f54458774740dfb958ae9160f47383
Andy Martin <andy@andymartin.cc>
2015-03-31 19:36:54 -0700

    clean & reorganize robo-browser directory

--- robo-browser/robo-browser.py
+++ robo-browser/robo-browser.py
@@ -10,7 +10,6 @@ import sys
 import time
 
 import selenium
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -29,7 +28,7 @@ driver.set_page_load_timeout(N_SECS_REQ_TIMEOUT)
 
 while True:
     try:
-        # get next task from robotask API
+        # get the next task from the robotask API:
         response = requests.get(API_BASE_URL, 
                                 params=dict(q=json.dumps(dict(field='priority', direction='asc'))),
                                 headers={'Content-Type': 'application/json'},
@@ -40,25 +39,25 @@ while True:
 
         if len(task["objects"]) == 0:
             print("no jobs currently in the queue")
-            time.sleep(10)
+            time.sleep(20)
             continue
 
         priority = task["objects"][0]["priority"]
         url = task["objects"][0]["url"]
-        id = task["objects"][0]["id"]
-        print("got task:", id, priority, url)
+        task_id = task["objects"][0]["id"]
+        print("got task for url:", url)
 
-        # go fetch the page in our browser
+        # remove the job from the queue
+        response = requests.delete("{0}/{1}".format(API_BASE_URL, task_id), verify=False)
+        assert response.status_code == 204
+
+        # go fetch the page in the selenium webdriver
         web_addr = str(url)
         driver.get(web_addr)
         time.sleep(N_SECS_TO_WAIT_AFTER_ONLOAD)
 
-        # remove our job from the queue
-        response = requests.delete("{0}/{1}".format(API_BASE_URL, id), verify=False)
-        assert response.status_code == 204
-
     except selenium.common.exceptions.TimeoutException:
-        print("the page load timed out for {0} - continuing...".format(web_addr))
+        print("the page load timed out for {0} - continuing on...".format(web_addr))
 
 driver.quit()
 
--- robo-browser/setup-headless-environment.sh
+++ robo-browser/setup-headless-environment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #
+# run this script before launching robo-browser.py to have the robo-browser run
+# headlessly (ie over a ssh connection)
+#
+# requires:
+#  - xvfb 
+#
 
 export PATH_TO_EXTENSION=/home/andy/projects/ScriptObservatory/chrome-extension/
 Xvfb :1 -screen 0 1024x768x24 2>&1 > /dev/null &
