a24efe5fa07b2708a0f88e568b60101416c2fc47
Andy Martin <andy@andymartin.cc>
2015-04-07 01:00:12 +0000

    make roboplotter run every 8h

--- backend/helper-scripts/robo-plotter.sh
+++ backend/helper-scripts/robo-plotter.sh
@@ -4,10 +4,13 @@
 while :
 do
     date 
-
     ./plot-total-observations-over-time.py /home/andy/projects/ScriptObservatory/backend/static/img/
+
+    sleep 300  # 5 minutes
     ./plot-observations-per-hour.py /home/andy/projects/ScriptObservatory/backend/static/img/
 
-    sleep 12000
+
+    sleep 28800  # 8 hours
+
 done
 
