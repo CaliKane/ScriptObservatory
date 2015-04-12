9585f118cb051be6d710a555565cd8941331f440
Andy Martin <andy@andymartin.cc>
2015-04-12 17:04:28 +0000

    remove unused code from backendy.py (old count_entires API, DELETE method for ScriptContent API, some imports)

--- backend/backend.py
+++ backend/backend.py
@@ -4,8 +4,8 @@
 from flask import Flask
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import Column, Integer, Text, ForeignKey, create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, Text, ForeignKey
 
 
 app = Flask(__name__, static_url_path='')
@@ -66,7 +66,7 @@ api_manager.create_api(RoboTask,
 
 api_manager.create_api(ScriptContent,
                        max_results_per_page=0,
-                       methods=["GET", "POST", "DELETE", "PUT"])
+                       methods=["GET", "POST", "PUT"])
 
 api_manager.create_api(ScriptUrlIndex,
                        max_results_per_page=0,
@@ -80,17 +80,6 @@ api_manager.create_api(ScriptHashIndex,
 @app.route('/')
 def index():
     return app.send_static_file("index.html")
-
-@app.route('/api/count_entries')
-def count_entries():
-    some_engine = create_engine("sqlite:///database.db")
-    Session = sessionmaker(bind=some_engine)
-    session = Session()
-
-    n_scripts = session.query(Script).count()
-    n_pageviews = session.query(Pageview).count()
-    return "{0} {1}".format(n_scripts, n_pageviews)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, use_reloader=False)
