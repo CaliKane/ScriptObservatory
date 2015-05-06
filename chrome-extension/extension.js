3999a5dcb4a944dfc62ac62b03f69a1da5a6962b
Andy Martin <andy@andymartin.cc>
2015-05-05 22:22:00 -0700

    re-enable script-content reporting

--- chrome-extension/extension.js
+++ chrome-extension/extension.js
@@ -137,7 +137,7 @@ chrome.webRequest.onBeforeRequest.addListener(
                 var script_content_data = {"sha256": hash, 
                                            "content": data};
                 
-                //httpPost(SCRIPTCONTENT_API_URL, script_content_data);      
+                httpPost(SCRIPTCONTENT_API_URL, script_content_data);      
             }
 
             var data_uri = window.btoa(unescape(encodeURIComponent(data)));
@@ -186,7 +186,7 @@ chrome.tabs.onUpdated.addListener(
                     var script_content_data = {"sha256": hash, 
                                                "content": data};
             
-                    //httpPost(SCRIPTCONTENT_API_URL, script_content_data);
+                    httpPost(SCRIPTCONTENT_API_URL, script_content_data);
                 }
 
                 var timeStamp = new Date().getTime();
