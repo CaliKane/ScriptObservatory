1701fed29ccfe3177731cc78c714e22af0eb1b45
Andy Martin <andy@andymartin.cc>
2015-05-05 21:59:52 -0700

    add standalone script-content API

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 #
 
+import json
+import os
+import hashlib
 import time
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
@@ -73,6 +76,46 @@ api_manager.create_api(RoboTask,
 api_manager.create_api(Suggestions,
                        max_results_per_page=0,
                        methods=["GET", "POST", "PUT"])
+
+
+SCRIPT_CONTENT_FOLDER = "/home/andy/projects/ScriptObservatory/backend/static/script-content/"
+
+@app.route('/script-content/<path:filename>', methods=["GET"])
+def get_script_content(filename):
+    return send_from_directory(SCRIPT_CONTENT_FOLDER, "{0}.txt".format(filename), as_attachment=False)
+
+
+@app.route('/script-content', methods=["POST"])
+def post_script_content():
+    # first we check to see if the file exists for the hash the client provides (let's
+    # avoid spending the effort to hash the user's data in the 99% case where the user 
+    # isn't lying. we'll eventually check their hash calculation later)
+    req = json.loads(str(request.data, 'utf-8'))
+
+    sha256_c = req.get('sha256')
+    filename = os.path.join(SCRIPT_CONTENT_FOLDER, '{0}.txt'.format(sha256_c))
+    if os.path.isfile(filename):
+        # TODO sanitize this filepath from path traversals... ok for now because we're just 
+        # checking for the existance of a file, but longer-term we should not allow this information 
+        # disclosure
+        print('we already have this content')
+        return 'we already have this content'
+
+    content = req.get('content')
+    sha256 = hashlib.sha256(content.encode('utf-8')).hexdigest()
+
+    # we double check that the hash we've calculated matches the hash provided by the client
+    # before writing the file to disk
+    # TODO: auto-report cases where this check fails? something strange is going on if we get here...
+    if sha256_c != sha256:
+        return "content / hash mismatch"
+
+    # ok, now we can finally write the file
+    filename = os.path.join(SCRIPT_CONTENT_FOLDER, '{0}.txt'.format(sha256))
+    with open(filename, 'w') as f:
+        f.write(content)
+
+    return 'thanks!'
 
 
 @app.route('/search', methods=["GET"])
