2288977f05c6a20aabf1cfe8d14553f9fbdfc259
Andy Martin <andy@andymartin.cc>
2015-05-17 18:41:54 -0700

    add scriptobservatory.org to hosts file via .travis.yml file, test robotask api query in first-test.py

--- misc/tests/first-test.py
+++ misc/tests/first-test.py
@@ -3,8 +3,17 @@
 # first test for nose
 #
 
+import requests
+
+
 def test_test():
     print("testing!")
 
+def test_get_robotasks():
+    r = requests.get("https://scriptobservatory.org/api/robotask",
+                     headers={'content-type': 'application/json'})
+
+    print(r.status_code, r.data)
+    assert r.status_code == 200
 
 
