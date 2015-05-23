5d5243916ed9e820774f746eb138059fd471b978
Andy Martin <andy@andymartin.cc>
2015-05-23 23:41:15 +0000

    add thank you to micah & notes about how to manually create indices for the database

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 #
+# This code implements all backend functionality, including database models, database 
+# management, advanced data querying, and script-content file serving.
+
+# NOTES on speeding up queries....
+#   Table indices need to be manually created until code is added to do this directly 
+#   with flask/SQLAlchemy. Information here describes the command you need to run:
+#
+#        https://www.sqlite.org/lang_createindex.html
+#
+#    Thanks Micah for the pointers! :) https://github.com/macro1
+#
 
 import json
 import os
