9951779ce32d2e04e16aed0c9cbab12d313fb26e
Andy Martin <andy@andymartin.cc>
2015-05-25 01:00:16 +0000

    automatically ignore 'date' keys when making json comparisons

--- tests/tests.py
+++ tests/tests.py
@@ -219,8 +219,10 @@ def check_search(url, expected):
 def ordered(obj):
     """ https://stackoverflow.com/questions/25851183/how-to-compare-two-json-objects-with-the-same-elements-in-a-different-order-equa """
     if isinstance(obj, dict):
-        return sorted((k, ordered(v)) for k, v in obj.items())
-    if isinstance(obj, list):
+        return sorted((k, ordered(v)) for k, v in obj.items() if k != 'date')
+    elif isinstance(obj, list):
         return sorted(ordered(x) for x in obj)
     else:
         return obj
+
+
