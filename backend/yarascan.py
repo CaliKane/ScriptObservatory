5e25735a3702165d1953b0397a5cf6088408276d
Andy Martin <andy@andymartin.cc>
2015-06-20 19:34:47 +0000

    clean up yarascan code

--- backend/yarascan.py
+++ backend/yarascan.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 #
 
-import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from requests import get
 from html import escape
 from os import environ
 from random import randint
@@ -10,20 +12,28 @@ from subprocess import check_output, CalledProcessError
 from sys import argv
 from time import sleep, time
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-
-SCRIPT_CONTENT_URL = "https://scriptobservatory.org/script-content/{0}"
-QUERY_URL  = "https://scriptobservatory.org/api/search?script_by_hash={0}"  
 
 MAX_HASHES = 500
 MAX_PAGES_PER_HASH = 25
+YARA_SCAN_TIMEOUT = 1500
+
+EMAIL_WHITELIST = environ['EMAIL_WHITELIST'].split(',')
+
+SCRIPT_QUERY_URL  = "https://scriptobservatory.org/api/search?script_by_hash={0}"  
+
+SUCCESS_SUBJECT = "YARA Scan Results (success!)"
+SUCCESS_MESSAGE = "<html><head></head><body><u>Query:</u><br>{0}<br><br><u>Warning:</u><br>{1}</u><br><br><u>Hits:</u><br>{2}<u>Elapsed Time:</u><br>{3} seconds</body></html>"
+
+FAILURE_SUBJECT = "YARA Scan Results (error!)"
+FAILURE_MESSAGE = "<html><head></head><body>Query:<br>{0}<br><br>Error:<br>{1}<br><br>Check your YARA syntax!</body></html>"
+
+WARNING_MESSAGE = """A maximum of {0} hashes and {1} webpages for each hash will be shown here. 
+                  To see full results, refine your YARA rule or query the server directly.""".format(MAX_HASHES, MAX_PAGES_PER_HASH)
 
 
 def sendmail(dest_addr, subject, message):
-    """ send an email with content *message* to *dest_addr* using the external SMTP server
-        specified with environment variables GMAIL_ACCOUNT / GMAIL_PASSWORD. """
+    """ send an email with subject/content *subject*/*message* to *dest_addr* using
+        the credentials specified with env variables GMAIL_ACCOUNT & GMAIL_PASSWORD. """
     src_addr = environ["GMAIL_ACCOUNT"]
 
     msg = MIMEMultipart('alternative')
@@ -49,56 +59,60 @@ def drop_rule_file(text):
 
 
 def run_yara_scan(dst_email, rule):
-    TIMEOUT = 1200
-    WHITELIST = environ['EMAIL_WHITELIST'].split(',')
-
     search_directory = "/home/andy/projects/ScriptObservatory/backend/static/script-content/"  # move to env var
 
-    if dst_email not in WHITELIST: 
+    if dst_email not in EMAIL_WHITELIST: 
         print("{0} not in email whitelist!".format(dst_email))
         sendmail(environ["GMAIL_ACCOUNT"], "Rejected YARA Scan", "{0} is not in the whitelist!".format(dst_email))
         exit(1)
 
     tmp_file = drop_rule_file(rule)
+    
     try:
         start_t = time()
-        output = check_output("nice -n 1 yara --threads=1 {0} {1}".format(tmp_file, search_directory), shell=True, timeout=TIMEOUT)
+        output = check_output("nice -n 8 yara --threads=1 {0} {1}".format(tmp_file, search_directory), shell=True, timeout=YARA_SCAN_TIMEOUT)
         end_t = time()
+
         output = output.decode("utf-8")
         output = output.replace("{0}/".format(search_directory), "")
         output = output.replace(".txt", "")
         output = output.split('\n')
 
         if len(output) == 0:
-            output = "no results found"
-        elif len(output) > MAX_HASHES:
-            output = output[:MAX_HASHES]
-
-        final_output = ""
-        for o in output:
-            if o == '': continue
-            sleep(0.1)
-            h = o[-64:]  # the hash is the last 64 characters of *o*
-            
-            r = requests.get(QUERY_URL.format(h), verify=False)
-            assert r.status_code == 200
-            
-            results = r.json()['objects']
-            if len(results) > MAX_PAGES_PER_HASH: 
-                results = results[:MAX_PAGES_PER_HASH] 
-            results = ['<a href="https://scriptobservatory.org/search/?query={0}" target="_blank">{0}</a>, '.format(r) for r in results]
-
-            final_output += "Hash {0} matches, seen on:<br>{1}<br><br>".format('<a href="https://scriptobservatory.org/search/?query={0}" target="_blank">{0}</a>'.format(h), " ".join(results))
+            final_output = "no results found"
         
-        warning = "A maximum of {0} hashes and {1} webpages for each hash will be shown here. To see full results, refine your YARA rule or query the server directly.".format(MAX_HASHES, MAX_PAGES_PER_HASH)
-        subject = "YARA Scan Results (success!)"
-        message = "<html><head></head><body><u>Query:</u><br>{0}<br><br><u>Warning:</u><br>{1}</u><br><br><u>Hits:</u><br>{2}<u>Elapsed Time:</u><br>{3} seconds</body></html>".format(escape(rule), warning, final_output, end_t-start_t)
+        else:
+            if len(output) > MAX_HASHES:
+                output = output[:MAX_HASHES]
+
+            final_output = ""
+            for match in output:
+                if match == '': continue
+                sleep(0.1)
+                h = match[-64:]  # the hash is the last 64 characters of *match*
+                
+                r = get(SCRIPT_QUERY_URL.format(h), verify=False)
+                assert r.status_code == 200
+                
+                results = r.json()['objects']
+                if len(results) > MAX_PAGES_PER_HASH: 
+                    results = results[:MAX_PAGES_PER_HASH] 
+                results = ['<a href="https://scriptobservatory.org/search/?query={0}" target="_blank">{0}</a>, '.format(r) for r in results]
+
+                final_output += "Hash {0} matches, seen on:<br>{1}<br><br>".format('<a href="https://scriptobservatory.org/search/?query={0}" target="_blank">{0}</a>'.format(h), 
+                                                                                   ' '.join(results))
+        
+        subject = SUCCESS_SUBJECT
+        message = SUCCESS_MESSAGE.format(escape(rule).replace('\n', '<br>'), 
+                                         WARNING_MESSAGE, 
+                                         final_output,
+                                         end_t-start_t)
 
     except CalledProcessError as e:
-        print(e)
         output = str(e)
-        subject = "YARA Scan Results (error!)"
-        message = "Query:\n {0}\n\nError:\n{1}\n\nCheck your YARA syntax!".format(escape(rule), output)
+        subject = FAILURE_SUBJECT
+        message = FAILURE_MESSAGE.format(escape(rule).replace('\n', '<br>'),
+                                         output)
 
     sendmail(dst_email, subject, message)
     
