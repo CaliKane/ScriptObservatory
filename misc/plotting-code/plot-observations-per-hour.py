ddc21c337f50c1035861221b7198cdc291451f3c
Andy Martin <andy@andymartin.cc>
2015-08-16 23:52:46 +0000

    update/refactor plotting code to work with pg

--- misc/plotting-code/plot-observations-per-hour.py
+++ misc/plotting-code/plot-observations-per-hour.py
@@ -1,54 +1,34 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 
-import json
+import datetime
 import os
-import requests
-import sqlite3
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pylab import *
-
 axes = figure().add_subplot(111)
 
-N_X_LABELS = 5
-N_DATA_POINTS = 40
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from backend.models import Webpage, Pageview, Resource
+
+
 OUTPUT_BASEDIR = sys.argv[1]  #/static/img/ directory
 
-
-
-conn = sqlite3.connect('../../backend/database.db')
-
-c = conn.cursor()
-
-pageviews = list(c.execute("SELECT * FROM pageview"))
-# format: id, url, time
-
-pv_times = []
-
-t_first = int(pageviews[0][2])
-t_last = int(pageviews[-1][2])
-t_elapsed = t_last - t_first
-time_vals = range(t_first, t_last, t_elapsed/N_DATA_POINTS)
-hours_per_tv = float(t_elapsed/N_DATA_POINTS) / (1000*60*60)
-
 y = []
-labels = []
+x = []
+start_t = end_t = datetime.datetime(2015, 4, 1)
+while True:
+    start_t = end_t
+    end_t += datetime.timedelta(days=1)
+    
+    if end_t > datetime.datetime.now(): break
 
-time_vals_ind = 0
-obs_so_far = 0
-for n, pv in enumerate(pageviews):
-    t = int(pv[2])
-    obs_so_far += 1  # len(pv["scripts"])
-
-    if len(time_vals) > time_vals_ind and t > time_vals[time_vals_ind]:
-        #print obs_so_far / hours_per_tv
-        y.append(obs_so_far * 0.001 / hours_per_tv)
-        labels.append(t)
-        time_vals_ind += 1
-        obs_so_far = 0
+    views = Pageview.query.filter(Pageview.date < end_t).filter(Pageview.date >= start_t).all()
+    
+    y.append(len(views) / 1000)
+    x.append(start_t)
 
 plt.plot(range(len(y)), y, 'r-')
 plt.title("New Pageview Observations Added Per Hour")
@@ -56,18 +36,16 @@ plt.ylabel("Thousands of pageviews")
 
 xmin, xmax, ymin, ymax = plt.axis()
 plt.axis([xmin, xmax, 0, ymax])
+
+n_entries = len(x)
 n_labels = len(axes.get_xticklabels())
-
-time_labels = range(t_first + t_elapsed/n_labels, t_last + t_elapsed/n_labels, t_elapsed/n_labels)
-
-for i in range(len(time_labels)):
-    time_labels[i] = datetime.datetime.fromtimestamp(time_labels[i]/1000).strftime('%m/%d')
+time_labels = []
+for i in range(n_entries):
+    if i % (n_entries // n_labels) == (n_entries // n_labels) - 1:
+        time_labels.append(x[i].strftime('%m/%d'))
 time_labels[0] = ""
-
 axes.set_xticklabels(time_labels)
 
 os.system("rm {0}/new-entries-per-hour.png".format(OUTPUT_BASEDIR))
 plt.savefig(OUTPUT_BASEDIR + "/new-entries-per-hour.png")
-
-
 
--- misc/plotting-code/plot-unique-webpages-over-time.py
+++ misc/plotting-code/plot-unique-webpages-over-time.py
@@ -1,72 +1,54 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 
-import json
+from collections import defaultdict
+import datetime
 import os
-import requests
-import sqlite3
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pylab import *
-
 axes = figure().add_subplot(111)
 
-N_X_LABELS = 5
-N_DATA_POINTS = 250
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from backend.models import Webpage, Pageview, Resource
+
+
 OUTPUT_BASEDIR = sys.argv[1]  #/static/img/ directory
 
-
-
-conn = sqlite3.connect('../../backend/database.db')
-
-c = conn.cursor()
-
-pageviews = list(c.execute("SELECT * FROM pageview"))
-# format: id, url, time
-
-pv_times = []
-
-t_first = int(pageviews[0][2])
-t_last = int(pageviews[-1][2])
-t_elapsed = t_last - t_first
-time_vals = range(t_first, t_last, t_elapsed/N_DATA_POINTS)
-hours_per_tv = float(t_elapsed/N_DATA_POINTS) / (1000*60*60)
-
+seen_urls = defaultdict(lambda: None, {})
 y = []
-labels = []
-URL_LIST = []
-
-time_vals_ind = 0
-obs_so_far = 0
-for n, pv in enumerate(pageviews):
-    url = pv[1]
-    t = int(pv[2])
+x = []
+start_t = end_t = datetime.datetime(2015, 4, 1)
+while True:
+    start_t = end_t
+    end_t += datetime.timedelta(days=1)
     
-    if url not in URL_LIST:
-        obs_so_far += 1  # len(pv["scripts"])
-        URL_LIST.append(url)
+    if end_t > datetime.datetime.now(): break
 
-    if t > time_vals[time_vals_ind]:
-        #print obs_so_far / hours_per_tv
-        y.append(obs_so_far/1000.0)
-        labels.append(t)
-        time_vals_ind += 1
-
+    views = Pageview.query.filter(Pageview.date < end_t).filter(Pageview.date >= start_t).all()
+    for v in views:
+        if seen_urls[v.url] is None:
+            seen_urls[v.url] = True
+    
+    y.append(len(seen_urls) / 1000)
+    x.append(start_t)
+ 
 plt.plot(range(len(y)), y, 'r-')
 plt.title("Unique Webpages in Database")
-plt.ylabel("1,000s of webpages")
+plt.ylabel("Thousands of Webpages")
+
 xmin, xmax, ymin, ymax = plt.axis()
-plt.axis([xmin, xmax, 0, ymax+4])
+plt.axis([xmin, xmax, 0, ymax])
+
+n_entries = len(x)
 n_labels = len(axes.get_xticklabels())
-
-time_labels = range(t_first+t_elapsed/n_labels, t_last+t_elapsed/n_labels, t_elapsed/n_labels)
-
-for i in range(len(time_labels)):
-    time_labels[i] = datetime.datetime.fromtimestamp(time_labels[i]/1000).strftime('%m/%d')
+time_labels = []
+for i in range(n_entries):
+    if i % (n_entries // n_labels) == (n_entries // n_labels) - 1:
+        time_labels.append(x[i].strftime('%m/%d'))
 time_labels[0] = ""
-
 axes.set_xticklabels(time_labels)
 
 os.system("rm {0}/unique-webpages-over-time.png".format(OUTPUT_BASEDIR))
