5956b2dfb8362c58d75fce6ff3a31126d1c4118e
Andy Martin <andy@andymartin.cc>
2015-10-10 16:06:24 +0000

    add yara_scan_file.delay() call when a resource_content is requested to rescan previously seen content. closes #42

--- backend/views.py
+++ backend/views.py
@@ -251,6 +251,13 @@ def resource_content_api_get():
                         content = html.escape(content)
             else:
                 content = 'true'
+                yara_scan_file.delay(filename)  # adding this .delay() call here
+                                                # is not ideal, but works for now.
+                                                # we want to re-scan hashes when
+                                                # they're observed for the 2nd, 3rd, etc
+                                                # time, but the POST requests will
+                                                # not be sent for repeat observations!
+                                                # adding this here, for now.
         response[sha256] = content
 
     if beautify:
