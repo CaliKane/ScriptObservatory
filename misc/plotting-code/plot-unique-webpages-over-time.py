2f5088a91fc94086e720bc51925bbff277dfd234
CaliKane <accardo209@yahoo.com>
2015-10-25 02:23:17 -0700

    Updated plot-unique-webpages-over-time.py
    Made changes in order to achieve desired plotting on graph.

--- misc/plotting-code/plot-unique-webpages-over-time.py
+++ misc/plotting-code/plot-unique-webpages-over-time.py
@@ -17,15 +17,18 @@ from backend.models import Webpage, Pageview, Resource
 
 OUTPUT_BASEDIR = sys.argv[1]  #/static/img/ directory
 
-seen_urls = defaultdict(lambda: None, {})
+
 y = []
 x = []
-start_t = end_t = datetime.datetime(2015, 4, 1)
+
+seen_urls = defaultdict(lambda: None, {})
+end_t = datetime.datetime(2015, 4, 1)
 while True:
     start_t = end_t
     end_t += datetime.timedelta(days=2)
     
-    if end_t > datetime.datetime.now(): break
+    if end_t > datetime.datetime.now(): 
+        break
 
     views = Pageview.query.filter(Pageview.date < end_t).filter(Pageview.date >= start_t).all()
     for v in views:
@@ -34,23 +37,17 @@ while True:
     
     y.append(len(seen_urls) / 1000)
     x.append(start_t)
- 
-plt.plot(range(len(y)), y, 'r-')
+
+plt.plot(x, y, 'r-')
 plt.title("Unique Webpages in Database")
 plt.ylabel("Thousands of Webpages")
 
 xmin, xmax, ymin, ymax = plt.axis()
 plt.axis([xmin, xmax, 0, ymax])
 
-n_entries = len(x)
-n_labels = len(axes.get_xticklabels()) - 2
-time_labels = []
-for i in range(n_entries):
-    if i % (n_entries // n_labels) == 0:
-        time_labels.insert(0, x[-1 - i].strftime('%m/%d'))
-time_labels[0] = ""
-axes.set_xticklabels(time_labels)
+dateFmt = matplotlib.dates.DateFormatter('%m/%d')
+axes.xaxis.set_major_formatter(dateFmt)
+axes.xaxis.get_major_ticks()[0].label1.set_visible(False)
 
 os.system("rm {0}/unique-webpages-over-time.png".format(OUTPUT_BASEDIR))
 plt.savefig(OUTPUT_BASEDIR + "/unique-webpages-over-time.png")
-
